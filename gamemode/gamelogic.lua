25c7040ab6d1a66e81704e91e4b5f34a3ca4b84e
John Woodruff <rocketpanda40@gmail.com>
2020-11-28 18:59:49 -0500

    Add bombs as entities instead of strings; check for endstate (1 bomb remaining) and call End() function when reached; initial implementation of End()

--- gamemode/gamelogic.lua
+++ gamemode/gamelogic.lua
@@ -1,11 +1,25 @@
 if SERVER then
     local bombs = {}
     
-    function AddBomb(name)
-        table.insert(bombs, name)
+    function AddBomb(bomb)
+        table.insert(bombs, bomb)
     end
     
-    function DelBomb(name)
-        table.remove(bombs, table.KeyFromValue(bombs, name))
+    function DelBomb(bomb)
+        table.remove(bombs, table.KeyFromValue(bombs, bomb))
+        if #bombs == 1 then End(bombs[1].team) end
+    end
+
+    function End(team)
+        if team then PrintMessage(4, team.." has won!")
+        else PrintMessage(4, "Round ended") end
+
+        if !table.IsEmpty(bombs) then
+            for key, bomb in pairs(bombs) do
+                bomb:Remove()
+            end
+        end
+
+        table.Empty(bombs)
     end
 end
