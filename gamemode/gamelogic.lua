5ed68868ddbb8a3c5a2a8c796ce25118206abe88
kerbalcadet <kerbalcadet@gmail.com>
2020-11-29 01:38:23 -0600

    added temp printbombs function

--- gamemode/gamelogic.lua
+++ gamemode/gamelogic.lua
@@ -1,20 +1,22 @@
-if SERVER then
-    local bombs = {}
+local bombs = {}
     
-    function AddBomb(bomb)
-        table.insert(bombs, bomb)
-    end
-    
-    function DelBomb(bomb)
-        table.remove(bombs, table.KeyFromValue(bombs, bomb))
-        if #bombs == 1 then End(bombs[1].team) end
-    end
+function AddBomb(bomb)
+    table.insert(bombs, bomb)
+end
 
-    function End(team)
-        if team then PrintMessage(4, team.." has won!")
-        else PrintMessage(4, "Round ended") end
+function DelBomb(bomb)
+    table.remove(bombs, table.KeyFromValue(bombs, bomb))
+    if #bombs == 1 then End(bombs[1].team) end
+end
 
-        table.Empty(bombs)
-        game.CleanUpMap(false)
-    end
+function End(team)
+    if team then PrintMessage(4, team.." has won!")
+    else PrintMessage(4, "Round ended") end
+
+    table.Empty(bombs)
+    game.CleanUpMap(false)
+end
+
+function PrintBombs()               --TEMP
+    print(table.ToString(bombs))
 end
