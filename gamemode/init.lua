c11a20813ac31b6b1e5dad3a3f081743aa31bb75
kerbalcadet <kerbalcadet@gmail.com>
2020-12-06 15:48:07 -0600

    fixed include directories

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,13 +1,13 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
-AddCSLuaFile("./content/fonts.lua")
+AddCSLuaFile("../content/fonts.lua")
 
 include("shared.lua")
 include("player.lua")
 include("config/commands.lua")
 include("config/config.lua")
 include("gamelogic.lua")
-include("./content/sound.lua")
+include("../content/sound.lua")
 
 math.randomseed(CurTime())
 SetActiveTeams()
