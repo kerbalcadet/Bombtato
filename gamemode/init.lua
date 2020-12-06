d83db32c459487cb49a87ef77a394555db2bb004
kerbalcadet <kerbalcadet@gmail.com>
2020-12-06 15:41:04 -0600

    moved sound a font to content

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,13 +1,13 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
-AddCSLuaFile("config/fonts.lua")
+AddCSLuaFile("./content/fonts.lua")
 
 include("shared.lua")
 include("player.lua")
 include("config/commands.lua")
 include("config/config.lua")
 include("gamelogic.lua")
-include("sound.lua")
+include("./content/sound.lua")
 
 math.randomseed(CurTime())
 SetActiveTeams()
