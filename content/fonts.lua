da717d12b7c39f9537cc612f782663468e473d50
kerbalcadet <kerbalcadet@gmail.com>
2020-12-06 15:40:51 -0600

    moved sound a font to content

---
+++ content/fonts.lua
@@ -0,0 +1,13 @@
+surface.CreateFont("FONT", {
+    font = "Arial",
+    size = 100,
+    weight = 1000,
+    shadow = false
+})
+
+surface.CreateFont("FONTSMALL", {
+    font = "Arial",
+    size = 50,
+    weight = 1000,
+    shadow = false
+})
