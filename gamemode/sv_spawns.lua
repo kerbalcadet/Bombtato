12ab30c8bb5761f568fbbbd29c4a562f97138288
John Woodruff <rocketpanda40@gmail.com>
2020-12-27 17:17:18 -0500

    Remove team from active round when bomb detonated - remove debug spawns if applicable and make players spectators (invisible, god mode, no weapons, can't arm/defuse).

--- gamemode/sv_spawns.lua
+++ gamemode/sv_spawns.lua
@@ -1,5 +1,6 @@
 local bspawns = {}
 local tspawns = {}
+local dbgspawns = {}
 
 local function SortSpawns(bomb, spawns)
     local tbl = spawns
@@ -13,14 +14,18 @@ local function SortSpawns(bomb, spawns)
 end
 
 function BOMB:InitSpawns()
+    -- init
     table.Empty(bspawns)
     table.Empty(tspawns)
+    table.Empty(dbgspawns)
 
     local spawns = ents.FindByClass("info_player_*")
     local nt = BOMB_NUMTEAMS:GetInt()
     local spi = math.random(1, #spawns)
     local sp = spawns[spi]
+    -- end init
 
+    -- set bomb spawns
     table.insert(bspawns, sp)
     table.remove(spawns, spi)
 
@@ -32,7 +37,14 @@ function BOMB:InitSpawns()
         table.insert(bspawns, spawns[curspi])
         table.remove(spawns, curspi)
     end
+    -- end set bomb spawns
 
+    -- set spectator spawns (all but bomb spawns)
+    table.insert(tspawns, TEAM_SPECTATOR, {})
+    for _, spawn in pairs(spawns) do table.insert(tspawns[TEAM_SPECTATOR], spawn) end
+    -- end set spectator spawns
+
+    -- set team spawns
     local ti = 1
     while(not table.IsEmpty(spawns)) do
         if not tspawns[ti] then tspawns[ti] = {} end
@@ -41,6 +53,7 @@ function BOMB:InitSpawns()
         table.remove(spawns, table.KeyFromValue(spawns, dtable[1]))
         ti = ti % nt + 1
     end
+    -- end set team spawns
 end
 
 function BOMB:DebugSpawns()
@@ -48,24 +61,27 @@ function BOMB:DebugSpawns()
     local tsp = BOMB:GetTeamSpawns()
 
     for _, spawn in pairs(ents.FindByClass("info_player_*")) do
-        local spmdl = ents.Create("prop_physics")
-        spmdl:PhysicsInit(SOLID_NONE)
-        spmdl:SetCollisionGroup(COLLISION_GROUP_WORLD)
-        spmdl:SetPos(spawn:GetPos())
-        spmdl:SetModel("models/props_wasteland/medbridge_post01.mdl")
-        spmdl:SetMaterial("models/debug/debugwhite")
+        if not table.HasValue(bspawns, spawn) then
+            local spmdl = ents.Create("prop_physics")
+            spmdl:PhysicsInit(SOLID_NONE)
+            spmdl:SetCollisionGroup(COLLISION_GROUP_WORLD)
+            spmdl:SetPos(spawn:GetPos())
+            spmdl:SetModel("models/props_wasteland/medbridge_post01.mdl")
+            spmdl:SetMaterial("models/debug/debugwhite")
 
-        -- set models to be transparent until discovered in team spawns, so bomb spawns are not visible
-        spmdl:SetColor(Color(0, 0, 0, 0))
-        spmdl:SetRenderMode(RENDERMODE_TRANSCOLOR)
-        
-        for i = 1, #tsp do
-            for j = 1, #(tsp[i]) do
-                if tsp[i][j] == spawn then spmdl:SetColor(team.GetColor(i)) break end
+            for i = 1, #tsp do
+                if not dbgspawns[i] then dbgspawns[i] = {} end
+                for j = 1, #(tsp[i]) do
+                    if tsp[i][j] == spawn then
+                        spmdl:SetColor(team.GetColor(i))
+                        table.insert(dbgspawns[i], spmdl)
+                        break
+                    end
+                end
             end
+            
+            spmdl:Spawn()
         end
-        
-        spmdl:Spawn()
     end
 end
 
@@ -75,4 +91,14 @@ end
 
 function BOMB:GetTeamSpawns()
     return tspawns
+end
+
+function BOMB:DelTeamSpawns(teamindex)
+    table.Empty(tspawns[teamindex]) -- we don't actually remove the table, because that shifts the other team spawn tables by index and messes up the other teams' spawnpoints
+    if BOMB_DBG_SPAWNS:GetBool() then
+        for _, spmdl in pairs(dbgspawns[teamindex]) do
+            spmdl:Remove()
+        end
+        table.Empty(dbgspawns[teamindex]) -- same story here as ^
+    end
 end
